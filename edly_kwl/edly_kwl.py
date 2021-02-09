23d12033cfc88818360fcae9064b25b3f93e42a8
qasimgulzar <pent007#@>
2021-02-09 14:32:58 +0500

    ♻️ EDE-1354 clean example code.

--- edly_kwl/edly_kwl.py
+++ edly_kwl/edly_kwl.py
@@ -1,7 +1,7 @@
 """Know Wonder Learn Activity XBlock"""
 
 from xblock.core import XBlock
-from xblock.fields import String, Scope
+from xblock.fields import String, Scope, List
 from web_fragments.fragment import Fragment
 
 from edly_kwl.utils import render_template, resource_string
@@ -15,15 +15,9 @@ class EdlyKWLXBlock(XBlock):
     display_name = String(help="This name appears in horizontal navigation at the top of the page.",
                           default="Edly KWL",
                           scope=Scope.content)
-    know_text = String(help="Enter details about Know columns description",
-                       default="Step1, Enter things that you already know about this topic",
-                       scope=Scope.content)
-    wonder_text = String(help="Enter details about Know columns description",
-                         default="Step 2, Enter things that you wonder about this topic",
-                         scope=Scope.content)
-    learned_text = String(help="Enter details about Learned columns description",
-                          default="Step 3, Move the things that you have learned during this training to Learned column",
-                          scope=Scope.content)
+    know_text = List(help="Enter details about Know columns description", default=[], scope=Scope.user_info)
+    wonder_text = List(help="Enter details about Know columns description", default=[], scope=Scope.user_info)
+    learned_text = List(help="Enter details about Learned columns description", default=[], scope=Scope.user_info)
 
     # Context argument is specified for xblocks, but we are not using herein
     def student_view(self, context=None):   # pylint: disable=unused-argument
@@ -40,19 +34,6 @@ class EdlyKWLXBlock(XBlock):
         fragment.add_javascript(resource_string("static/js/src/edly_kwl.js"))
         fragment.initialize_js('EdlyKWLXBlock')
         return fragment
-
-    # TO-DO: change this handler to perform your own actions.  You may need more
-    # than one handler, or you may not need any handlers at all.
-    @XBlock.json_handler
-    def increment_count(self, data, suffix=''):
-        """
-        An example handler, which increments the data.
-        """
-        # Just to show data coming in...
-        assert data['hello'] == 'world'
-
-        self.count += 1
-        return {"count": self.count}
 
     # TO-DO: change this to create the scenarios you'd like to see in the
     # workbench while developing your XBlock.
