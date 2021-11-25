88ae9e466ac20026dd144ab1d71e1124a14195f9
Muhammad Usman <91720383+m-usman01@users.noreply.github.com>
2021-11-25 14:41:22 +0500

    PKX-663 Changed Edly to ilmX (#29)

--- edly_kwl/edly_kwl.py
+++ edly_kwl/edly_kwl.py
@@ -24,7 +24,7 @@ class EdlyKWLXBlock(XBlock):
     has_score = True
     icon_class = 'problem'
     display_name = String(help="This name appears in horizontal navigation at the top of the page.",
-                          default="Edly KWL", scope=Scope.settings)
+                          default="ilmX KWL", scope=Scope.settings)
     config = Any(scope=Scope.settings, default={
         'know_title': 'Know',
         'wonder_title': 'Wonder',
